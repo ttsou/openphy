538a2347bc55287759f7cba2aecc08edbbd4b9cb
Tom Tsou <tom.tsou@ettus.com>
2017-06-18 15:42:05 -0700

    pdsch: Remove non-existent allocate-deallocate calls

--- src/lte/pdsch.h
+++ src/lte/pdsch.h
@@ -1,9 +1,7 @@
 #ifndef _LTE_PDSCH_
 #define _LTE_PDSCH_
 
-struct lte_pdsch_decoder;
 struct lte_subframe;
-struct lte_dci;
 struct lte_pdsch_blk;
 struct lte_time;
 
@@ -11,9 +9,5 @@ int lte_decode_pdsch(struct lte_subframe **subframe,
 		     int chans, struct lte_pdsch_blk *blk,
 		     int cfi, int dci_index,
 		     struct lte_time *time);
-
-struct lte_pdsch_decoder *lte_pdsch_alloc_decoder(int d_len, int e_len);
-
-void lte_pdsch_free_decoder(struct lte_pdsch_decoder *dec);
 
 #endif /* LTE_PDSCH_ */
